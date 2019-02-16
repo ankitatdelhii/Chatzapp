22007896454d2db9a77d38979fa542af8d74eb71
Ankit Saxena <ankitatdelhii@ankits-macbook-air.local>
2019-02-16 21:22:30 +0530

    Login View Added! AutoLayout Done!

---
+++ Chatzapp/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  Chatzapp
+//
+//  Created by Ankit Saxena on 16/02/19.
+//  Copyright © 2019 Ankit Saxena. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("In Login")
+    }
+    
+
+}
